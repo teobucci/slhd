6a750e42c2f85a6794435493d9590e5c092203ad
teobucci <teobucci8@gmail.com>
2023-04-27 15:26:51 +0200

    Add title and authors

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# # Statistical Learning for Healthcare Data
+#
+# Authors:
+#
+# - Teo Bucci ([@teobucci](https://github.com/teobucci))
+# - Giulia Montani ([@GiuliaMontani](https://github.com/GiuliaMontani))
+# - Alice Traversa ([@AliceTraversa](https://github.com/AliceTraversa))
+
 import numpy as np
-
-# Prova
-
-# ## Prova titolo
+import pandas as pd
