df44972c2303b35e458634ba0f11afc51a2f19c1
teobucci <teobucci8@gmail.com>
2023-05-02 21:11:45 +0200

    Comment normalization part

--- main.py
+++ main.py
@@ -285,14 +285,14 @@ y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Standardize the features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-# Select the top k features using the ANOVA F-value
-k_best = SelectKBest(f_classif, k=10)
-X_train = k_best.fit_transform(X_train, y_train)
-X_test = k_best.transform(X_test)
+#scaler = StandardScaler()
+#X_train = scaler.fit_transform(X_train)
+#X_test = scaler.transform(X_test)
+#
+## Select the top k features using the ANOVA F-value
+#k_best = SelectKBest(f_classif, k=10)
+#X_train = k_best.fit_transform(X_train, y_train)
+#X_test = k_best.transform(X_test)
 # -
 
 # # Imputing missing values
