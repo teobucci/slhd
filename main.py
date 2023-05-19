1d8ebe08880754b9ae489fba4c3db0d4c9630b63
teobucci <teobucci8@gmail.com>
2023-05-19 17:09:51 +0200

    Remove code

--- main.py
+++ main.py
@@ -1090,23 +1090,7 @@ for idx, col_name in enumerate(col_inspect):
 
 fig.tight_layout()
 plt.show()
-
-# +
-#df_temp = df.copy()
-#df_temp['all'] = ''
-#sns.violinplot(x='all', y='weight', hue=target_var, data = df_temp, split=True)
-#plt.xlabel("")
-#plt.show()
 # -
-
-# Sono 119 variabili è perciò difficile visualizzarle tutte in un unico blocco. Potremmo forse fare blocchi con una decina di variabili max che siano "simili" tra loro. 
-# Come parametro per dire quanto sono simili potremmo sia usare la correlazione che vedere effettivamente cosa descrivono (i.e. weight, hight, BMI nello stesso blocco) TODO
-
-# Discrete variable visualization
-
-discrete_vars = [col for col in df.columns if df[col].dtype == 'int64']
-for var in discrete_vars:
-    print('Unique values of the discrete variable',var, 'are: ',sorted(df[var].unique()))
 
 # ### Correlation analysis
 
